e042854dbc7c1c7fa9449d0b743c759e4932ccb7
chpancrate <chpancrate@gmail.com>
2023-04-14 16:32:54 +0200

    add local storage and partial css and images

--- movies.js
+++ movies.js
@@ -1,11 +1,4 @@
-/*
-const query = await fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score,title");
-const jsonQuery = await query.json();
-const movies = jsonQuery.results
-console.log(movies);
-*/
-
-function displayCategory(start, moviesList, category){
+function displayCategory(start, moviesList, category) {
     /* Display 4 movies from the movieList starting from start
       in the category zone */
     var end = start + 4;
@@ -26,17 +19,18 @@ function displayCategory(start, moviesList, category){
         movieArticle.appendChild(nameMovie);
         movieArticle.appendChild(imageMovie);
         category.appendChild(movieArticle);
-        
+
     }
 }
 
 
-function displayBestFilm(movieList){
+function displayBestFilm(movieList) {
     /* display the best film */
+    console.log("BF:", movieList[0])
     const sectionBestFilm = document.querySelector("#best_film");
     const movieArticle = document.createElement("article")
     const nameMovie = document.createElement("p");
-    nameMovie.innerText = movieList[0].title;
+    nameMovie.innerText = movieList[0]["title"];
     const imageMovie = document.createElement("img");
     imageMovie.src = movieList[0].image_url
     movieArticle.appendChild(nameMovie);
@@ -44,69 +38,57 @@ function displayBestFilm(movieList){
     sectionBestFilm.appendChild(movieArticle);
 }
 
-async function retrieveMoviesList(url, url2, category){
+async function retrieveMoviesList(url, url2, category) {
     let query = await fetch(url);
     let jsonQuery = await query.json();
     let movies1 = jsonQuery.results;
-    let query2 = await fetch(url2) 
+    let query2 = await fetch(url2)
     let jsonQuery2 = await query2.json();
     let movies2 = jsonQuery2.results;
     let moviesList = movies1.concat(movies2)
     console.log(typeof category);
     console.log(typeof moviesList);
     console.log(category, moviesList);
-    if (category === "#best_film"){
-        displayBestFilm(moviesList);
-        const sectionOtherBestFilms = document.querySelector("#other_best_films");
-        displayCategory(1, moviesList, sectionOtherBestFilms);
-    }else{
-        console.log(typeof category);
-        console.log(category);
-        const sectionCategory = document.querySelector(category);
-        displayCategory(0, moviesList, sectionCategory)
-    }
+    window.localStorage.setItem(category, JSON.stringify(moviesList));
 }
 
-/* display the best films */
-let url1="http://localhost:8000/api/v1/titles/?sort_by=-imdb_score,title";
-let url2="http://localhost:8000/api/v1/titles/?Page=2&sort_by=-imdb_score,title";
-retrieveMoviesList(url1, url2, "#best_film");
+/* retrieve data from database and store in local storage*/
+let url1 = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score,title";
+let url2 = "http://localhost:8000/api/v1/titles/?Page=2&sort_by=-imdb_score,title";
+retrieveMoviesList(url1, url2, "best_films");
+
+url1 = "http://localhost:8000/api/v1/titles/?genre=Sci-Fi&sort_by=-imdb_score,title";
+url2 = "http://localhost:8000/api/v1/titles/?Page=2&genre=Sci-Fi&sort_by=-imdb_score,title";
+retrieveMoviesList(url1, url2, "category1");
+
+url1 = "http://localhost:8000/api/v1/titles/?genre=Comedy&sort_by=-imdb_score,title";
+url2 = "http://localhost:8000/api/v1/titles/?Page=2&genre=Comedy&sort_by=-imdb_score,title";
+retrieveMoviesList(url1, url2, "category2");
+
+url1 = "http://localhost:8000/api/v1/titles/?genre=Adventure&sort_by=-imdb_score,title";
+url2 = "http://localhost:8000/api/v1/titles/?Page=2&genre=Adventure&sort_by=-imdb_score,title";
+retrieveMoviesList(url1, url2, "category3");
+
+/* display Bests films*/
+let moviesList = JSON.parse(window.localStorage.getItem("best_films"))
+displayBestFilm(moviesList);
+
+let sectionCategory = document.querySelector("#other_best_films");
+displayCategory(1, moviesList, sectionCategory);
 
 /* display category 1 */
-url1="http://localhost:8000/api/v1/titles/?genre=Sci-Fi&sort_by=-imdb_score,title";
-url2="http://localhost:8000/api/v1/titles/?Page=2&genre=Sci-Fi&sort_by=-imdb_score,title";
-retrieveMoviesList(url1, url2, "#category1");
+moviesList = JSON.parse(window.localStorage.getItem("category1"))
+console.log("cat1", moviesList)
+sectionCategory = document.querySelector("#category1");
+displayCategory(0, moviesList, sectionCategory)
 
 /* display category 2 */
-url1="http://localhost:8000/api/v1/titles/?genre=Comedy&sort_by=-imdb_score,title";
-url2="http://localhost:8000/api/v1/titles/?Page=2&genre=Comedy&sort_by=-imdb_score,title";
-retrieveMoviesList(url1, url2, "#category2");
+moviesList = JSON.parse(window.localStorage.getItem("category2"))
+sectionCategory = document.querySelector("#category2");
+displayCategory(0, moviesList, sectionCategory)
 
 /* display category 3 */
-url1="http://localhost:8000/api/v1/titles/?genre=Adventure&sort_by=-imdb_score,title";
-url2="http://localhost:8000/api/v1/titles/?Page=2&genre=Adventure&sort_by=-imdb_score,title";
-retrieveMoviesList(url1, url2, "#category3");
+moviesList = JSON.parse(window.localStorage.getItem("category3"))
+sectionCategory = document.querySelector("#category3");
+displayCategory(0, moviesList, sectionCategory)
 
-
-/*
-const cat1Query = await fetch("http://localhost:8000/api/v1/titles/?genre=Sci-Fi&sort_by=-imdb_score,title");
-const jsonCat1Query = await cat1Query.json();
-const cat1Movies = jsonCat1Query.results
-console.log(cat1Movies);
-const sectionCategory1 = document.querySelector("#category1");
-displayCategory(0, cat1Movies, sectionCategory1)
-
-const cat2Query = await fetch("http://localhost:8000/api/v1/titles/?genre=Comedy&sort_by=-imdb_score,title");
-const jsonCat2Query = await cat2Query.json();
-const cat2Movies = jsonCat2Query.results
-console.log(cat2Movies);
-const sectionCategory2 = document.querySelector("#category2");
-displayCategory(0, cat2Movies, sectionCategory2)
-
-const cat3Query = await fetch("http://localhost:8000/api/v1/titles/?genre=Adventure&sort_by=-imdb_score,title");
-const jsonCat3Query = await cat3Query.json();
-const cat3Movies = jsonCat3Query.results
-console.log(cat3Movies);
-const sectionCategory3 = document.querySelector("#category3");
-displayCategory(1, cat3Movies, sectionCategory3)
-*/
